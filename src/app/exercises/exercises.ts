fb672f420774c566279a7bf381b4f630ca867a84
June Kim <zhxxne@gmail.com>
2021-11-29 15:04:31 +0100

    Initial commit

---
+++ src/app/exercises/exercises.ts
@@ -0,0 +1,12 @@
+import { Exercises } from './types';
+
+export const exercisesList: Exercises = [
+    { name: 'Creating Observables', folder: 'creating' },
+    { name: 'Window resize: Observables from events', folder: 'fromevent' },
+    { name: 'Multicasting with Subjects', folder: 'multicast' },
+    { name: 'Error handling', folder: 'error-handling' },
+    { name: 'How to unsubscribe and avoid memory leaks', folder: 'unsubscribe' },
+    { name: 'Higher Order Observables', folder: 'higherorder' },
+    { name: 'Extra: Chat: Merging Observables', folder: 'chat' },
+    { name: 'Extra: Drag and drop', folder: 'dragdrop' },
+];
